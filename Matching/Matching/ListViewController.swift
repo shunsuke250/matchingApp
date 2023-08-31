b62f908f5ac8454cf71cf60be05b826870071a83
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-01 00:57:00 +0900

    :sparkles: アカウント作成機能を実装

---
+++ Matching/Matching/ListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ListViewController.swift
+//  Matching
+//
+//  Created by 副山俊輔 on 2023/08/28.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("listViewController is displayed")
+        view.backgroundColor = .cyan
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Matching/Matching/ViewController.swift
+++ Matching/Matching/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     private let emailTextField: UITextField = {
         $0.borderStyle = .roundedRect
         $0.placeholder = "example@example.com"
+        $0.autocapitalizationType = .none // 最初の文字を大文字にしない
         return $0
     }(UITextField())
     
@@ -27,17 +28,24 @@ class ViewController: UIViewController {
         return $0
     }(UILabel())
     
+    private let createAccountButton: UIButton = {
+        $0.setTitle("アカウント作成", for: .normal)
+        $0.backgroundColor = .systemCyan
+        return $0
+    }(UIButton())
+    
     private let passwordTextField: UITextField = {
         $0.borderStyle = .roundedRect
         $0.isSecureTextEntry = true
         return $0
     }(UITextField())
     
-    private let createAccountButton: UIButton = {
-        $0.setTitle("アカウント作成", for: .normal)
-        $0.backgroundColor = .systemCyan
+    private let errorLabel: UILabel = {
+        $0.numberOfLines = 2 // 複数行表示を許可
+        $0.lineBreakMode = .byWordWrapping // 単語単位で折り返す
+        $0.sizeToFit()
         return $0
-    }(UIButton())
+    }(UILabel())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,28 +57,25 @@ class ViewController: UIViewController {
         createAccountButton.addAction(.init { [weak self] _ in
             guard let email = self?.emailTextField.text,
                   let password = self?.passwordTextField.text else { return }
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                if let error = error {
-                    print("Error creating user: \(error.localizedDescription)")
-                } else {
-                    // 新しいユーザーアカウントが作成された場合の処理をここに追加
-                    print("User created successfully.")
-                }
-            }
+            self?.createAccount(email: email, password: password)
+            let listViewController = ListViewController()
+            let navigationController = UINavigationController(rootViewController: listViewController)
+            self?.present(navigationController, animated: true, completion: nil)
         }, for: .touchUpInside)
     }
-        
+    
     
     func setConstrains() {
         view.addSubview(emailLabel)
         view.addSubview(emailTextField)
         view.addSubview(passwordLabel)
         view.addSubview(passwordTextField)
+        view.addSubview(errorLabel)
         view.addSubview(createAccountButton)
         
         emailLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().offset(200)
+            $0.top.equalToSuperview().offset(140)
         }
         emailTextField.snp.makeConstraints {
             $0.centerX.equalToSuperview()
@@ -80,7 +85,7 @@ class ViewController: UIViewController {
         }
         passwordLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(emailTextField.snp.bottom).offset(50)
+            $0.top.equalTo(emailTextField.snp.bottom).offset(30)
         }
         passwordTextField.snp.makeConstraints {
             $0.centerX.equalToSuperview()
@@ -88,10 +93,31 @@ class ViewController: UIViewController {
             $0.leftMargin.equalTo(30)
             $0.rightMargin.equalTo(30)
         }
+        errorLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(10)
+            $0.leftMargin.equalTo(30)
+            $0.rightMargin.equalTo(30)
+        }
         createAccountButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(passwordTextField.snp.bottom).offset(50)
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(60)
             $0.width.greaterThanOrEqualTo(150)
+        }
+    }
+    
+    func createAccount(email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.errorLabel.text = ("\(error.localizedDescription)")
+                print("Error creating user: \(error.localizedDescription)")
+            } else {
+                // 新しいユーザーアカウントが作成された場合の処理をここに追加
+                print("User created successfully.")
+                self.errorLabel.text = ""
+                let listViewController = ListViewController()
+                self.navigationController?.pushViewController(listViewController, animated: true)
+            }
         }
     }
 }
@@ -100,11 +126,11 @@ class ViewController: UIViewController {
 extension UIViewController {
     struct PreviewWrapper: UIViewControllerRepresentable {
         let viewController: UIViewController
-
+        
         func makeUIViewController(context: Context) -> UIViewController {
             return viewController
         }
-
+        
         func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         }
     }
